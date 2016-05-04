2ab678e5e1d69bd441b2e30feca135fb303d7f36
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2016-05-04 00:38:40 -0300

    Start Project 4

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Curso Angular by Codecademy
 
 Veja os projetos <a href="http://angular.oceanoweb.com.br/" target="_blank">AQUI</a>
+
+### Observação
+
+Instalar Node HTTP-Server
+
+```
+npm install http-server -g
+```
+Execute
+
+```
+http-server c:\location\to\app
+```
---
+++ project-4/js/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('GalleryApp', ['ngRoute']);
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'HomeController',
+      templateUrl: 'views/home.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
